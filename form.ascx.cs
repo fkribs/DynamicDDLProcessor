0e5f594e842b745a0ffe5095add1104f136fecc6
fkribs <ffkribs@gmail.com>
2019-04-15 12:42:54 -0600

    Update form.ascx.cs

--- form.ascx.cs
+++ form.ascx.cs
@@ -7,7 +7,7 @@
             SPListItemCollection items;
             try
             {
-                items = SMMasterUtilities.GetSPListItems((Control)sender);
+                items = OrgUtilitiesClass.GetSPListItems((Control)sender);
             }
             catch
             {
@@ -16,12 +16,12 @@
 
             foreach (SPListItem item in items)
             {
-                string itemCode = SMMasterUtilities.GetItemValue(item, "Code");
-                //string itemName = SMMasterUtilities.GetItemValue(item, "Name");
+                string itemCode = OrgUtilitiesClass.GetItemValue(item, "Code");
+                //string itemName = OrgUtilitiesClass.GetItemValue(item, "Name");
                 //if ((itemCode == selectedText || itemCode == selectedValue) || (itemName == selectedText || itemName == selectedValue)) //if ddl text or value matches SP list code or name.
                 if (itemCode == selectedValue)
                 {
-                    SMMasterUtilities.ProcessItem(item, this);
+                    OrgUtilitiesClass.ProcessItem(item, this);
                     break;
                 }
             }
